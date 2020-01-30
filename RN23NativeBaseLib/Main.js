a39feb88f1da9e9ade178d8831fd28d2da52e866
tjrrms0809 <tjrrms0809@nate.com>
2020-01-30 15:09:55 +0900

    123

--- RN23NativeBaseLib/Main.js
+++ RN23NativeBaseLib/Main.js
@@ -1,59 +1,78 @@
-import React, {Componet} from 'react';
-import { Container, Header, Content, Left, Button, Icon, Body, Footer, Title, FooterTab, Text, Card, CardItem, Thumbnail } from 'native-base';
+import React, {Component} from 'react';
+import {Container, Header, Content, Footer,Left, Button, Icon, Body, Title, Right, FooterTab,Text, Card, CardItem, Thumbnail} from 'native-base';
+import {Image} from 'react-native';
 
 export default class Main extends Component{
-    render() {
-        // Container : 자동 flex:1 스타일이 적용된 View
-        <Container style={{backgroundColor:"green"}}>
-            {/* 크게 3개영역으로 화면 구성 */}
-            <Header>
-                <Left>
+    render(){
+        return (
+            // Container : 자동 flex:1 스타일이 적용된 View
+            <Container>
+                {/* 크게 3개영역으로 화면 구성 */}
+                <Header>
+                    <Left>
+                        <Button>
+                            <Icon name="menu"></Icon>
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Native Base</Title>
+                    </Body>
+                    <Right>                        
+                    </Right>
+                </Header>
+
+                <Content style={{padding:16}}>
+                    <Button warning><Text>button</Text></Button>
+                    <Button info bordered><Text>button</Text></Button>
+                    <Button dark rounded><Text>button</Text></Button>
+                    <Button block danger><Text>button</Text></Button>
                     <Button>
-                        <Icon name="menu"></Icon>
+                        <Icon name="home"></Icon>
+                        <Text>HOME</Text>
                     </Button>
-                </Left>
-                <Body>
-                    <Title>Native Base</Title>
-                </Body>
-            </Header>
 
-            <Content style={{padding:16}}>
-                <Button warning><Text>button</Text></Button>
-                <Button info bordered><Text>button</Text></Button>
-                <Button dark rounded><Text>button</Text></Button>
-                <Button block danger><Text>button</Text></Button>
-                <Button>
-                    <Icon name="home"></Icon>
-                    <Text>HOME</Text>
-                </Button>
+                    <Card>
+                        <CardItem>
+                            <Text>Navtive base</Text>
+                        </CardItem>
+                        <CardItem button onPress={()=>alert('clicked')}>
+                            <Body>
+                                <Text>click on any carditem</Text>
+                            </Body>                            
+                        </CardItem>
+                    </Card>
 
-                <Card>
-                    <CardItem>
-                        <Thumbnail source={{uri:'https://i.ytimg.com/vi/wCSz1ApV9-k/maxresdefault.jpg'}}></Thumbnail>
-                        <Text>Native base</Text>
-                    </CardItem>
-                    <CardItem button onPress={()=>alert('click')}>
-                        <Body>
-                            <Text>clic on any carditem</Text>
-                        </Body>
-                    </CardItem>
-                </Card>
-            </Content>
+                    <Card>
+                        <CardItem>
+                            <Thumbnail source={{uri: 'https://i.ytimg.com/vi/wCSz1ApV9-k/maxresdefault.jpg'}}></Thumbnail>
+                            <Text>Navtive base</Text>
+                        </CardItem>
+                        <CardItem button onPress={()=>alert('clicked')}>
+                            <Body>
+                                <Text>click on any carditem</Text>
+                            </Body>                            
+                        </CardItem>
+                    </Card>
 
-            <Footer>
-                {/* Bottom 탭 */}
-                <FooterTab>
-                    <Button>
-                        <Text>TAB1</Text>
-                    </Button>
-                </FooterTab>
-                <FooterTab>
-                    <Button>
-                        <Text>TAB2</Text>
-                    </Button>
-                </FooterTab>
-            </Footer>
 
-        </Container>
+                </Content>
+
+                <Footer>
+                    {/* Bottom 탭 */}
+                    <FooterTab>
+                        <Button>
+                            <Text>TAB1</Text>
+                        </Button>
+                    </FooterTab>
+                    <FooterTab>
+                        <Button>
+                            <Text>TAB2</Text>
+                        </Button>
+                    </FooterTab>
+
+                </Footer>
+                
+            </Container>
+        );
     }
 }
